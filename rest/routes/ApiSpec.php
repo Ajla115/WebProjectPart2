fd2ed1af0438c907090c4c7487788ca98774f2db
Ajla Korman <ajla.korman@stu.ibu.edu.ba>
2023-07-09 23:28:00 +0200

    Added actual link to deployment

--- rest/routes/ApiSpec.php
+++ rest/routes/ApiSpec.php
@@ -5,7 +5,7 @@
  * @OA\Info(title="Web project", version="0.1", @OA\Contact(email="demo@gmail.com", name="Web Project"))
  * @OA\OpenApi(
  *    @OA\Server(url="http://localhost/WebProjectPart2/rest", description="Development Environment" ),
- *    @OA\Server(url="https://WebProjectPart2.ba/rest", description="Production Environment" )
+ *    @OA\Server(url="https://sea-lion-app-2bkxh.ondigitalocean.app/rest", description="Production Environment" )
  * ),
  * @OA\SecurityScheme(securityScheme="ApiKeyAuth", type="apiKey", in="header", name="Authorization" )
  */
